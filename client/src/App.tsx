8dd67ba710a0ece8b596880547569d2aca78fa83
ChloeLeeFullStackDeveloper <chloe.lee0436@gmail.com>
2025-03-11 11:44:24 -0600

    edit frontend tailwind part

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,43 +7,45 @@ import TaxReport from "./pages/TaxReport";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PrivateRoute from "./components/auth/PrivateRoute";
-import Navbar from "./components/layout/Navbar";
+import MainLayout from "./components/layout/MainLayout";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <Navbar />
-        <div className="min-h-screen bg-gray-50">
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route 
-              path="/" 
+        <Routes>
+          {/* Public routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Protected routes with layout */}
+          <Route element={<MainLayout />}>
+            <Route
+              path="/"
               element={
                 <PrivateRoute>
                   <Dashboard />
                 </PrivateRoute>
-              } 
+              }
             />
-            <Route 
-              path="/tax-input" 
+            <Route
+              path="/tax-input"
               element={
                 <PrivateRoute>
                   <TaxInput />
                 </PrivateRoute>
-              } 
+              }
             />
-            <Route 
-              path="/tax-report" 
+            <Route
+              path="/tax-report"
               element={
                 <PrivateRoute>
                   <TaxReport />
                 </PrivateRoute>
-              } 
+              }
             />
-          </Routes>
-        </div>
+          </Route>
+        </Routes>
       </Router>
     </AuthProvider>
   );
